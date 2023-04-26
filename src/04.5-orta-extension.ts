044f257e1c97a2965281456cb54e6340fe2896d0
Matt Pocock <mattpocockvoice@gmail.com>
2023-04-26 10:40:36 +0100

    Changes

--- src/04.5-orta-extension.ts
+++ src/04.5-orta-extension.ts
@@ -1,6 +1,7 @@
 type ToUppercaseWithPrefix<T extends string> = `PREFIX:${Uppercase<T>}`;
 
 // Try using Orta's extension here!
+// Search for vscode-twoslash-queries
 
 type Example1 = ToUppercaseWithPrefix<"hello">;
 
